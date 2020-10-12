3fbae72de73bed194968c987ad030573e14aafbd
peng xi <xixioba@foxmail.com>
2020-10-12 11:05:41 +0800

    update head file thread

--- xp_logs.cpp
+++ xp_logs.cpp
@@ -29,311 +29,335 @@
 
 using namespace std;
 
-static void mkdirs(const char *__path, __mode_t __mode) {
-  if (strlen(__path) > PATH_MAX) {
-    return;
-  }
-  int ipathLength = strlen(__path);
-  int ileaveLength = 0;
-  int iCreatedLength = 0;
-  char szPathTemp[PATH_MAX] = {0};
+static void mkdirs(const char* __path, __mode_t __mode)
+{
+    if (strlen(__path) > PATH_MAX) {
+        return;
+    }
+    int ipathLength = strlen(__path);
+    int ileaveLength = 0;
+    int iCreatedLength = 0;
+    char szPathTemp[PATH_MAX] = { 0 };
 #if defined __linux__ || defined __APPLE__
-  for (int i = 0; (NULL != strchr(__path + iCreatedLength, '/')); i++)
+    for (int i = 0; (NULL != strchr(__path + iCreatedLength, '/')); i++)
 #elif defined _WIN32
-  for (int i = 0; (NULL != strchr(__path + iCreatedLength, '\\')); i++)
+    for (int i = 0; (NULL != strchr(__path + iCreatedLength, '\\')); i++)
 #endif
-  {
+    {
 #if defined __linux__ || defined __APPLE__
-    ileaveLength = strlen(strchr(__path + iCreatedLength, '/')) - 1;
+        ileaveLength = strlen(strchr(__path + iCreatedLength, '/')) - 1;
 #elif defined _WIN32
-    ileaveLength = strlen(strchr(__path + iCreatedLength, '\\')) - 1;
+        ileaveLength = strlen(strchr(__path + iCreatedLength, '\\')) - 1;
 #endif
-    iCreatedLength = ipathLength - ileaveLength;
+        iCreatedLength = ipathLength - ileaveLength;
 #if defined __linux__ || defined __APPLE__
-    if (ileaveLength + 1 == ipathLength) continue;
+        if (ileaveLength + 1 == ipathLength)
+            continue;
 #elif defined _WIN32
-    if (ileaveLength + 2 == ipathLength) continue;
+        if (ileaveLength + 2 == ipathLength)
+            continue;
 #endif
-    strncpy(szPathTemp, __path, iCreatedLength);
+        strncpy(szPathTemp, __path, iCreatedLength);
 #if defined __linux__ || defined __APPLE__
-    mkdir(szPathTemp, __mode);
+        mkdir(szPathTemp, __mode);
 #elif defined _WIN32
-    mkdir(szPathTemp);
+        mkdir(szPathTemp);
 #endif
-  }
-  if (iCreatedLength < ipathLength) {
+    }
+    if (iCreatedLength < ipathLength) {
 #if defined __linux__ || defined __APPLE__
-    mkdir(__path, __mode);
+        mkdir(__path, __mode);
 #elif defined _WIN32
-    mkdir(__path);
+        mkdir(__path);
 #endif
-  }
+    }
 }
 
-xp_logs::xp_logs(const char *path, const char *suffix, int daily, int cachesize)
-    : sub_thread() {
-  sprintf(logpath, "%s", path);
-  sprintf(Suffix, "%s", suffix);
-  Daily = daily;
-  if (cachesize > 0) {
-    Cachesize = cachesize;
-    cachebuff = new Ringbuf(cachesize);
-  }
+xp_logs::xp_logs(const char* path, const char* suffix, int daily, int cachesize)
+    : sub_thread()
+{
+    sprintf(logpath, "%s", path);
+    sprintf(Suffix, "%s", suffix);
+    Daily = daily;
+    if (cachesize > 0) {
+        Cachesize = cachesize;
+        cachebuff = new Ringbuf(cachesize);
+    }
 }
 
-xp_logs::~xp_logs() {
-  if (sub_thread.joinable()) sub_thread.join();
-  Cachesize = 0;
+xp_logs::~xp_logs()
+{
+    if (sub_thread.joinable())
+        sub_thread.join();
+    Cachesize = 0;
 }
 
 void xp_logs::run() { sub_thread = std::thread(&xp_logs::cachepool, this); };
-void xp_logs::cachepool(void) {
-  int n = 0;
-  while (Cachesize) {
-    n = cachebuff->canread();
-    if (n > 0)  //有数据
-    {
-      uint8_t *readbuff = new uint8_t[n];
-      cachebuff->read(readbuff, n);
-      this->add((char *)readbuff, n);
-      delete[] readbuff;
+void xp_logs::cachepool(void)
+{
+    int n = 0;
+    while (Cachesize) {
+        n = cachebuff->canread();
+        if (n > 0) //有数据
+        {
+            uint8_t* readbuff = new uint8_t[n];
+            cachebuff->read(readbuff, n);
+            this->add((char*)readbuff, n);
+            delete[] readbuff;
+        }
+        Delay(1);
     }
-    Delay(1);
-  }
-  cachebuff->~Ringbuf();
+    cachebuff->~Ringbuf();
 }
 
-int xp_logs::add_cache(char *str, int len) {
-  if (len > 0)
-    return cachebuff->add((uint8_t *)str, len);
-  else
-    return cachebuff->add((uint8_t *)str, strlen(str));
+int xp_logs::add_cache(char* str, int len)
+{
+    if (len > 0)
+        return cachebuff->add((uint8_t*)str, len);
+    else
+        return cachebuff->add((uint8_t*)str, strlen(str));
 }
 
-int xp_logs::list_all_files(char *basePath) {
-  DIR *dir;
-  struct dirent *ptr;
-  char base[1000];
-  if ((dir = opendir(basePath)) == NULL) {
-    perror("No such dir ");
-    exit(1);
-  }
-
-  while ((ptr = readdir(dir)) != NULL) {
-    if (strcmp(ptr->d_name, ".") == 0 ||
-        strcmp(ptr->d_name, "..") == 0)  /// current dir OR parrent dir
-      continue;
-    else if (ptr->d_type == 8)  /// file
-    {
-      // sprintf(tmp,"%s/%s %d\n",basePath,ptr->d_name);
-      printf("d_name:%s/%s %d\n", basePath, ptr->d_name, get_size(ptr->d_name));
-    } else if (ptr->d_type == 10)  /// link file
-      printf("d_name:%s/%s\n", basePath, ptr->d_name);
-    else if (ptr->d_type == 4)  /// dir
-    {
-      memset(base, '\0', sizeof(base));
-      strcpy(base, basePath);
-      strcat(base, "/");
-      strcat(base, ptr->d_name);
-      list_all_files(base);
+int xp_logs::list_all_files(char* basePath)
+{
+    DIR* dir;
+    struct dirent* ptr;
+    char base[1000];
+    if ((dir = opendir(basePath)) == NULL) {
+        perror("No such dir ");
+        exit(1);
     }
-  }
-  closedir(dir);
-  return 1;
+
+    while ((ptr = readdir(dir)) != NULL) {
+        if (strcmp(ptr->d_name, ".") == 0 || strcmp(ptr->d_name, "..") == 0) /// current dir OR parrent dir
+            continue;
+        else if (ptr->d_type == 8) /// file
+        {
+            // sprintf(tmp,"%s/%s %d\n",basePath,ptr->d_name);
+            printf("d_name:%s/%s %d\n", basePath, ptr->d_name, get_size(ptr->d_name));
+        } else if (ptr->d_type == 10) /// link file
+            printf("d_name:%s/%s\n", basePath, ptr->d_name);
+        else if (ptr->d_type == 4) /// dir
+        {
+            memset(base, '\0', sizeof(base));
+            strcpy(base, basePath);
+            strcat(base, "/");
+            strcat(base, ptr->d_name);
+            list_all_files(base);
+        }
+    }
+    closedir(dir);
+    return 1;
 }
 
-int xp_logs::list_files(char *path) {
-  struct dirent **name_list;
-  char tmp[1024];
-  int n = scandir(path, &name_list, 0, alphasort);
-  if (n < 0) {
-    printf("no such dir %s\n", path);
-  } else {
-    int index = 0;
-    while (index < n) {
-      sprintf(tmp, "%s/%s", path, name_list[index]->d_name);
-      // printf("name: %s %d\n", name_list[index]->d_name,get_size(tmp));
-      free(name_list[index++]);
+int xp_logs::list_files(char* path)
+{
+    struct dirent** name_list;
+    char tmp[1024];
+    int n = scandir(path, &name_list, 0, alphasort);
+    if (n < 0) {
+        printf("no such dir %s\n", path);
+    } else {
+        int index = 0;
+        while (index < n) {
+            sprintf(tmp, "%s/%s", path, name_list[index]->d_name);
+            // printf("name: %s %d\n", name_list[index]->d_name,get_size(tmp));
+            free(name_list[index++]);
+        }
+        free(name_list);
+    }
+    return n;
+}
+
+char* xp_logs::get_first_file(char* path)
+{
+    struct dirent** name_list;
+    int n = scandir(path, &name_list, 0, alphasort);
+    if (n < 0)
+        printf("no such dir %s\n", path);
+    else {
+        int index = 0;
+        char* name = NULL;
+        if (n > 2)
+            name = name_list[2]->d_name;
+        while (index < n)
+            free(name_list[index++]);
+        free(name_list);
+        return name;
     }
     free(name_list);
-  }
-  return n;
+    return NULL;
 }
 
-char *xp_logs::get_first_file(char *path) {
-  struct dirent **name_list;
-  int n = scandir(path, &name_list, 0, alphasort);
-  if (n < 0)
-    printf("no such dir %s\n", path);
-  else {
-    int index = 0;
-    char *name = NULL;
-    if (n > 2) name = name_list[2]->d_name;
-    while (index < n) free(name_list[index++]);
+char* xp_logs::get_last_file(char* path)
+{
+    struct dirent** name_list;
+    int n = scandir(path, &name_list, 0, alphasort);
+    if (n < 0)
+        printf("no such dir %s\n", path);
+    else {
+        int index = 0;
+        char* name = NULL;
+        if (n > 2)
+            name = name_list[n - 1]->d_name;
+        while (index < n)
+            free(name_list[index++]);
+        free(name_list);
+        return name;
+    }
     free(name_list);
-    return name;
-  }
-  free(name_list);
-  return NULL;
+    return NULL;
 }
 
-char *xp_logs::get_last_file(char *path) {
-  struct dirent **name_list;
-  int n = scandir(path, &name_list, 0, alphasort);
-  if (n < 0)
-    printf("no such dir %s\n", path);
-  else {
-    int index = 0;
-    char *name = NULL;
-    if (n > 2) name = name_list[n - 1]->d_name;
-    while (index < n) free(name_list[index++]);
-    free(name_list);
-    return name;
-  }
-  free(name_list);
-  return NULL;
-}
-
-int xp_logs::creat_log(char *path) {
-  FILE *fp = NULL;
-  static time_t timestamp = time(NULL);
-  char tmp[4096];
-  struct tm *t;
-  time_t now;
-  time(&now);
-  t = localtime(&now);
-  if (path == NULL)
-    sprintf(tmp, "%s/%4d-%02d-%02d %02d-%02d-%02d%s", logpath,
+int xp_logs::creat_log(char* path)
+{
+    FILE* fp = NULL;
+    static time_t timestamp = time(NULL);
+    char tmp[4096];
+    struct tm* t;
+    time_t now;
+    time(&now);
+    t = localtime(&now);
+    if (path == NULL)
+        sprintf(tmp, "%s/%4d-%02d-%02d %02d-%02d-%02d%s", logpath,
             t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min,
             t->tm_sec, Suffix);
-  else
-    sprintf(tmp, "%s/%s%s", logpath, path, Suffix);
-  if (access(logpath, 0) < 0) {
-    printf("create dir:%s\n", logpath);
+    else
+        sprintf(tmp, "%s/%s%s", logpath, path, Suffix);
+    if (access(logpath, 0) < 0) {
+        printf("create dir:%s\n", logpath);
 #if defined __linux__ || defined __APPLE__
-    mkdirs(logpath, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        mkdirs(logpath, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 #elif defined _WIN32
-    mkdirs(logpath);
+        mkdirs(logpath);
 #endif
-  }
-  fp = fopen(tmp, "a+");
-  if (fp == NULL) {
-    if (time(NULL) > timestamp + 1) {
-      timestamp = time(NULL);
-      printf("create wrong: %s\n", tmp);
+    }
+    fp = fopen(tmp, "a+");
+    if (fp == NULL) {
+        if (time(NULL) > timestamp + 1) {
+            timestamp = time(NULL);
+            printf("create wrong: %s\n", tmp);
+        }
+        fclose(fp);
+        return -1;
     }
     fclose(fp);
-    return -1;
-  }
-  fclose(fp);
-  return 0;
-}
-int xp_logs::get_size(char *filename) {
-  struct stat statbuf;
-  int size;
-  if (stat(filename, &statbuf) < 0) {
-    printf("get size wrong\n");
     return 0;
-  }
-  size = (statbuf.st_size / 1024);
-  size = size == 0 ? 1 : size;
-  return size;
 }
-
-int xp_logs::get_dir_size(char *path) {
-  int size = 0, index = 0;
-  struct dirent **name_list;
-  char dirpath[1024];
-  if (path == NULL)
-    sprintf(dirpath, "%s", logpath);
-  else
-    sprintf(dirpath, "%s/%s", logpath, path);
-  if (access(dirpath, 0) < 0) return -1;
-  int n = scandir(dirpath, &name_list, 0, alphasort);
-  while (index < n) {
-    char tmp[1024];
-    sprintf(tmp, "%s/%s", dirpath, name_list[index]->d_name);
-    size += get_size(tmp);
-    free(name_list[index++]);
-  }
-  free(name_list);
-  return size;
-}
-
-int xp_logs::writefile(char *path, const void *str, int len) {
-  static time_t timestamp = time(NULL);
-  char filepath[1024];
-  FILE *fp = NULL;
-  sprintf(filepath, "%s/%s", logpath, path);
-  fp = fopen(filepath, "ab");
-  if (fp == NULL) {
-    if (time(NULL) > timestamp + 1) {
-      timestamp = time(NULL);
-      printf("open file wrong %s\n", filepath);
+int xp_logs::get_size(char* filename)
+{
+    struct stat statbuf;
+    int size;
+    if (stat(filename, &statbuf) < 0) {
+        printf("get size wrong\n");
+        return 0;
     }
+    size = (statbuf.st_size / 1024);
+    size = size == 0 ? 1 : size;
+    return size;
+}
+
+int xp_logs::get_dir_size(char* path)
+{
+    int size = 0, index = 0;
+    struct dirent** name_list;
+    char dirpath[1024];
+    if (path == NULL)
+        sprintf(dirpath, "%s", logpath);
+    else
+        sprintf(dirpath, "%s/%s", logpath, path);
+    if (access(dirpath, 0) < 0)
+        return -1;
+    int n = scandir(dirpath, &name_list, 0, alphasort);
+    while (index < n) {
+        char tmp[1024];
+        sprintf(tmp, "%s/%s", dirpath, name_list[index]->d_name);
+        size += get_size(tmp);
+        free(name_list[index++]);
+    }
+    free(name_list);
+    return size;
+}
+
+int xp_logs::writefile(char* path, const void* str, int len)
+{
+    static time_t timestamp = time(NULL);
+    char filepath[1024];
+    FILE* fp = NULL;
+    sprintf(filepath, "%s/%s", logpath, path);
+    fp = fopen(filepath, "ab");
+    if (fp == NULL) {
+        if (time(NULL) > timestamp + 1) {
+            timestamp = time(NULL);
+            printf("open file wrong %s\n", filepath);
+        }
+        fclose(fp);
+        return -1;
+    }
+    fseek(fp, 0, SEEK_END);
+    fwrite(str, sizeof(char), len, fp);
     fclose(fp);
-    return -1;
-  }
-  fseek(fp, 0, SEEK_END);
-  fwrite(str, sizeof(char), len, fp);
-  fclose(fp);
-  return 0;
+    return 0;
 }
-int xp_logs::add(char *str, int len) {
-  int size;
-  char *name;
-  char tmp[1024];
-  int date[6];
-  if (list_files(logpath) > 2) {
-    size = get_dir_size(logpath);
-    if (size >= maxtotalsize)  //超过限制大小，删除最久远日志，添加新日志
-    {
-      name = get_first_file(logpath);
-      if (name) {
-        sprintf(tmp, "%s/%s", logpath, name);
-        printf("remove log %s\n", tmp);
-        remove(tmp);
-        add(str);
-      } else {
-        printf("get first file wrong\n");
-        return -1;
-      }
-    } else {
-      name = get_last_file(logpath);
-      if (Daily == 1) {
-        if (sscanf(name, "%4d-%02d-%02d %02d-%02d-%02d.log", &date[0], &date[1],
-                   &date[2], &date[3], &date[4], &date[5]) > 5) {
-          //判断是否同一天 t->tm_year + 1900, t->tm_mon + 1, t->tm_mday
-          struct tm *t;
-          time_t now;
-          time(&now);
-          t = localtime(&now);
-          if ((t->tm_year + 1900) == date[0] && (t->tm_mon + 1) == date[1] &&
-              t->tm_mday == date[2])  //同一天
-            ;
-          else {
-            if (creat_log() >= 0) name = get_last_file(logpath);
-          }
-        }
-      }
-      if (name) {
-        sprintf(tmp, "%s/%s", logpath, name);
-        size = get_size(tmp);
-        if (size >= maxlogsize) {
-          if (creat_log() >= 0) add(str);
+int xp_logs::add(char* str, int len)
+{
+    int size;
+    char* name;
+    char tmp[1024];
+    int date[6];
+    if (list_files(logpath) > 2) {
+        size = get_dir_size(logpath);
+        if (size >= maxtotalsize) //超过限制大小，删除最久远日志，添加新日志
+        {
+            name = get_first_file(logpath);
+            if (name) {
+                sprintf(tmp, "%s/%s", logpath, name);
+                printf("remove log %s\n", tmp);
+                remove(tmp);
+                add(str);
+            } else {
+                printf("get first file wrong\n");
+                return -1;
+            }
         } else {
-          if (len > 0)
-            writefile(name, str, len);
-          else
-            writefile(name, str, strlen(str));
+            name = get_last_file(logpath);
+            if (Daily == 1) {
+                if (sscanf(name, "%4d-%02d-%02d %02d-%02d-%02d.log", &date[0], &date[1],
+                        &date[2], &date[3], &date[4], &date[5])
+                    > 5) {
+                    //判断是否同一天 t->tm_year + 1900, t->tm_mon + 1, t->tm_mday
+                    struct tm* t;
+                    time_t now;
+                    time(&now);
+                    t = localtime(&now);
+                    if ((t->tm_year + 1900) == date[0] && (t->tm_mon + 1) == date[1] && t->tm_mday == date[2]) //同一天
+                        ;
+                    else {
+                        if (creat_log() >= 0)
+                            name = get_last_file(logpath);
+                    }
+                }
+            }
+            if (name) {
+                sprintf(tmp, "%s/%s", logpath, name);
+                size = get_size(tmp);
+                if (size >= maxlogsize) {
+                    if (creat_log() >= 0)
+                        add(str);
+                } else {
+                    if (len > 0)
+                        writefile(name, str, len);
+                    else
+                        writefile(name, str, strlen(str));
+                }
+            } else {
+                printf("get last file wrong\n");
+                return -1;
+            }
         }
-      } else {
-        printf("get last file wrong\n");
-        return -1;
-      }
+    } else {
+        if (creat_log() >= 0)
+            add(str);
     }
-  } else {
-    if (creat_log() >= 0) add(str);
-  }
-  return 0;
+    return 0;
 }
--- xp_logs.h
+++ xp_logs.h
@@ -2,38 +2,36 @@
 #define XP_LOGS_H
 
 #include "xp_ringbuff.h"
-
+#include <thread>
 class xp_logs {
-  Ringbuf *cachebuff;
-  int Cachesize;
-  int Daily;
-  char logpath[512];
-  int maxtotalsize = 10 * 1024 * 1024;  //单位 K,默认，10*1024*1024=10GB
-  int maxlogsize = 1 * 1024 * 1024;     //单位 K，默认，1*1024*1024=1GB
-  char Suffix[512];
-  std::thread sub_thread;
+    Ringbuf* cachebuff;
+    int Cachesize;
+    int Daily;
+    char logpath[512];
+    int maxtotalsize = 10 * 1024 * 1024; //单位 K,默认，10*1024*1024=10GB
+    int maxlogsize = 1 * 1024 * 1024; //单位 K，默认，1*1024*1024=1GB
+    char Suffix[512];
+    std::thread sub_thread;
 
- public:
-  xp_logs(
-      const char *path, const char *suffix = ".log", int daily = 1,
-      int cachesize =
-          30 * 1024 *
-          1024);  // daily{0,1}:按天重置新日志,单位BYTE,cachesize=0,普通模式，cachesize>0,启用cachepool，数据将存入缓存中，稍后自动写入,保证缓存大小可靠，减少丢包
-  ~xp_logs();
-  void run();
-  int add(char *str, int len = 0);  // len==0,写入字符串，len>0，写入char *数组
-  int get_size(char *filename);         //获取文件大小
-  int get_dir_size(char *path = NULL);  //获取文件夹大小
-  int list_files(char *path);           //索引当前文件夹下所有文件
-  int list_all_files(char *basePath);   //索引所有文件，逐层
-  int creat_log(char *path = NULL);  //在默认路径下创建日期命名的文件或指定名称
-  char *get_first_file(char *path);  //获取指定目录下第一个文件名
-  char *get_last_file(char *path);  //获取指定目录下最后一个文件名
-  int writefile(char *path, const void *str, int len);
-  // cache mode，加入缓存机制，每1s操作一次disk
-  void cachepool(void);  //缓存循环
-  int add_cache(char *str,
-                int len = 0);  // len==0,写入字符串，len>0，写入char *数组
+public:
+    xp_logs(
+        const char* path, const char* suffix = ".log", int daily = 1,
+        int cachesize = 30 * 1024 * 1024); // daily{0,1}:按天重置新日志,单位BYTE,cachesize=0,普通模式，cachesize>0,启用cachepool，数据将存入缓存中，稍后自动写入,保证缓存大小可靠，减少丢包
+    ~xp_logs();
+    void run();
+    int add(char* str, int len = 0); // len==0,写入字符串，len>0，写入char *数组
+    int get_size(char* filename); //获取文件大小
+    int get_dir_size(char* path = NULL); //获取文件夹大小
+    int list_files(char* path); //索引当前文件夹下所有文件
+    int list_all_files(char* basePath); //索引所有文件，逐层
+    int creat_log(char* path = NULL); //在默认路径下创建日期命名的文件或指定名称
+    char* get_first_file(char* path); //获取指定目录下第一个文件名
+    char* get_last_file(char* path); //获取指定目录下最后一个文件名
+    int writefile(char* path, const void* str, int len);
+    // cache mode，加入缓存机制，每1s操作一次disk
+    void cachepool(void); //缓存循环
+    int add_cache(char* str,
+        int len = 0); // len==0,写入字符串，len>0，写入char *数组
 };
 
-#endif  // XP_LOGS_H
+#endif // XP_LOGS_H
